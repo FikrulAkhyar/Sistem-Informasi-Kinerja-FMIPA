618351bb1547ddb01e7ea8c3b52fa5fd5e5c3d72
FikrulAkhyar <72435247+FikrulAkhyar@users.noreply.github.com>
2023-01-01 17:08:16 +0700

    add favicon usk

--- app/Views/layouts/dashboard.php
+++ app/Views/layouts/dashboard.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Informasi Kinerja FMIPA USK</title>
     <link rel="stylesheet" href="<?= base_url('css/app.css') ?>">
+    <link rel="icon" type="image/x-icon" href="<?= base_url('favicon.ico') ?>">
     <script src="<?= base_url('js/app.js') ?>"></script>
     <script>
         const BASE_URL = "<?= base_url() ?>"
